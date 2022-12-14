b9b9a66435580dae01ed68aafb2de20affe202da
RockyBaura <251drougnov@gmail.com>
2022-12-14 20:58:07 +0600

    Limit only 1 dropdown open

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ const toggleHamMenu = document.querySelector('.nav__menu');
 toggleHamMenu.addEventListener('click', ()=>{
     nav.classList.toggle('active');
 })
+
+const dropdownLists = document.querySelectorAll(".nav__list li");
+
+dropdownLists.forEach((list) => {
+    list.addEventListener("click", () => {
+        if (list.classList.contains("open")) {
+            list.classList.remove("open");
+        } else {
+            dropdownLists.forEach((otherList) => {
+                if (otherList !== list && otherList.classList.contains("open")) {
+                    otherList.classList.remove("open");
+                }
+        });
+        list.classList.add("open");
+        }
+    });
+});
